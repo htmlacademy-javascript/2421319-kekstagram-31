68d5dcb4ad0318874130e9de99a4db37adb30a04
Keks <keks@htmlacademy.ru>
2024-03-20 11:21:57 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,4 @@
+import {photos} from './data.js';
+import {renderPhotos} from './picture.js';
+
+renderPhotos(photos);
---
+++ 8/js/util.js
@@ -0,0 +1,5 @@
+const getRandomInteger = (a, b) => Math.floor(Math.random() * (b - a + 1)) + a;
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export {getRandomInteger, getRandomArrayElement};
