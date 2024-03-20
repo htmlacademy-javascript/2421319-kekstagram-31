8e1ab9de7df8e3b46f5f057d4532b64dc090c6f1
VaselisaM <ralkina.vaselisa@gmail.com>
2024-03-20 20:57:12 +0500

    Открывается и закрывается

--- js/util.js
+++ js/util.js
@@ -2,4 +2,6 @@ const getRandomInteger = (a, b) => Math.floor(Math.random() * (b - a + 1)) + a;
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-export {getRandomInteger, getRandomArrayElement};
+const closeOnEscKeyDown = (evt) => evt.key === 'Escape';
+
+export {getRandomInteger, getRandomArrayElement, closeOnEscKeyDown};
